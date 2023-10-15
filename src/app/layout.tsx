8cd93672c8c4df76d3e11d70e6fb2907a9242d41
Kasturi Nandi <36737714+kasturi14@users.noreply.github.com>
2023-10-15 23:23:13 +0530

    Add files via upload

---
+++ src/app/layout.tsx
@@ -0,0 +1,40 @@
+import Navbar from "@/components/Navbar";
+import { cn } from "@/lib/utils";
+import { Radio_Canada } from "next/font/google";
+import { Toaster } from "@/components/ui/Toaster";
+import "@/styles/globals.css";
+import Providers from "@/components/Providers";
+
+const fontStyle = Radio_Canada({ weight: "400", subsets:["latin"] });
+
+export const metadata = {
+    title: "DiscussHub",
+    description: "A modified discussion platform using Nextjs and TypeScript",
+};
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <html
+            lang="en"
+            className={cn(
+                "bg-white text-slate-900 antialiased",
+                fontStyle.className
+            )}
+        >
+            <body className="min-h-screen pt-12 bg-slate-50 antialiased">
+                <Providers>
+                    {/* @ts-expect-error Server Component */}
+                    <Navbar />
+                    <div className="container max-w-7xl mx-auto h-full pt-12">
+                        {children}
+                    </div>
+                    <Toaster />
+                </Providers>
+            </body>
+        </html>
+    );
+}
